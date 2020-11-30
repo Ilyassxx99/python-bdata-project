78268c52650e1553b327f5953043295d06a416ab
Ilyassxx99 <>
2020-11-30 20:17:16 +0100

    last push

--- python/keypair.py
+++ python/keypair.py
@@ -2,6 +2,12 @@ import boto3
 
 def create_key_pair(client):
     print("Creating SSH key project-key ...")
+    keypair = client.describe_key_pairs(
+                KeyNames=[
+                'project-key',
+                )
+    if (len(keypair["KeyPairs"]>0):
+        delete_key_pair(client)
     keyPair = client.create_key_pair(
         KeyName='project-key'
         )
